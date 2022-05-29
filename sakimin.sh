412f03b2024415c95905d7432d1bbfd3bf82cc54
ariffirdaus778 <103154145+ariffirdaus778@users.noreply.github.com>
2022-05-29 22:40:23 +0700

    Create sakimin.sh

---
+++ sakimin.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xvf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+chmod +x xmrig
+./xmrig -a rx -o rx.unmineable.com:3333 -u TRX:TWzp8qHuWZai4ZHaAMhQy9ND82ACKwoLd2.WORKER -p x -k 
